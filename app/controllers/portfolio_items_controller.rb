38e87c77511cf40c1d010358be7f224395cdbe2f
KatyaEssina2 <56380670+KatyaEssina2@users.noreply.github.com>
2020-10-20 18:30:13 +0100

    some views

--- app/controllers/portfolio_items_controller.rb
+++ app/controllers/portfolio_items_controller.rb
@@ -33,4 +33,8 @@ class PortfolioItemsController < ApplicationController
             end
         end
     end
+
+    def show
+        @portfolio_item = PortfolioItem.find(params[:id])
+    end
 end
