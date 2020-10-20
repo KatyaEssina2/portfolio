c905052b8265368203671cf94ed58a2759b0f118
KatyaEssina2 <56380670+KatyaEssina2@users.noreply.github.com>
2020-10-20 18:13:03 +0100

    edit/update methods

--- app/controllers/portfolio_items_controller.rb
+++ app/controllers/portfolio_items_controller.rb
@@ -11,11 +11,26 @@ class PortfolioItemsController < ApplicationController
         @portfolio_item = PortfolioItem.new(params.require(:portfolio_item).permit(:title, :subtitle, :body))
 
         respond_to do |format|
-        if @portfolio_item.save
-            format.html { redirect_to portfolio_items_path, notice: 'Portfolio Item was successfully created.' }
-        else
-            format.html { render :new }
+            if @portfolio_item.save
+                format.html { redirect_to portfolio_items_path, notice: 'Portfolio Item was successfully created.' }
+            else
+                format.html { render :new }
+            end
         end
+    end
+
+    def edit
+        @portfolio_item = PortfolioItem.find(params[:id])
+    end
+
+    def update
+        @portfolio_item = PortfolioItem.find(params[:id])
+        respond_to do |format|
+            if @portfolio_item.update(params.require(:portfolio_item).permit(:title, :subtitle, :body))
+                format.html { redirect_to portfolio_items_path, notice: 'Item was successfully updated.' }
+            else
+                format.html { render :edit }
+            end
         end
     end
 end
