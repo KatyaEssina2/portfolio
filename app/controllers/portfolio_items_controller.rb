54c65b2bbbf11bcaa85a4bc42205687e74b17fd7
KatyaEssina2 <56380670+KatyaEssina2@users.noreply.github.com>
2020-10-20 18:03:13 +0100

    create actions for portfolios

--- app/controllers/portfolio_items_controller.rb
+++ app/controllers/portfolio_items_controller.rb
@@ -2,5 +2,20 @@ class PortfolioItemsController < ApplicationController
     def index
         @portfolio_items = PortfolioItem.all
     end
-end
 
+    def new
+        @portfolio_item = PortfolioItem.new
+    end
+
+    def create
+        @portfolio_item = PortfolioItem.new(params.require(:portfolio_item).permit(:title, :subtitle, :body))
+
+        respond_to do |format|
+        if @portfolio_item.save
+            format.html { redirect_to portfolio_items_path, notice: 'Portfolio Item was successfully created.' }
+        else
+            format.html { render :new }
+        end
+        end
+    end
+end
