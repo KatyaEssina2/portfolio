8a2ff2a92e3e507f14795f16a23fe5707739195a
KatyaEssina2 <56380670+KatyaEssina2@users.noreply.github.com>
2020-10-21 11:32:48 +0100

    custom toggle

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,13 @@ Rails.application.routes.draw do
 
   get 'about_me', to: 'pages#about'
   get 'contact_me', to: 'pages#contact'
-  
-  resources :blogs
+
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'pages#home'
