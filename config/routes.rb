676b48cc3bbbce84debbde9ddcfb7fed6f0d0fb7
KatyaEssina2 <56380670+KatyaEssina2@users.noreply.github.com>
2020-10-21 10:59:08 +0100

    add slugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :portfolio_items
+  resources :portfolio_items, except: [:show]
+  get 'portfolio/:id', to: 'portfolio_items#show', as: 'portfolio_show'
+
   get 'about_me', to: 'pages#about'
   get 'contact_me', to: 'pages#contact'
+  
   resources :blogs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
